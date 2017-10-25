4160f62edbeea8683cefcce76da3789586f3ad34
Alessandro Pietro Bardelli <apbard@users.noreply.github.com>
2017-10-25 21:36:13 +0200

    ENH: better versioning

--- orthoproj/__init__.py
+++ orthoproj/__init__.py
@@ -3,5 +3,9 @@ Orthoproj
 """
 
 from .orthogonal_projection import OrthoProj
+try:
+    from orthoproj.version import version as __version__
+except:
+    __version__ = "UNKNOWN"
 
 __all__ = ["OrthoProj"]
--- setup.py
+++ setup.py
@@ -1,18 +1,99 @@
-from os import path
+import os
+import subprocess
 from setuptools import setup
 # To use a consistent encoding
 from codecs import open
 
-here = path.abspath(path.dirname(__file__))
+
+MAJOR = 0
+MINOR = 0
+MICRO = 3
+ISRELEASED = True
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
+
+# Return the git revision as a string
+def git_version():
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+    except OSError:
+        GIT_REVISION = "Unknown"
+
+    return GIT_REVISION
+
+
+def get_version_info():
+    # Adding the git rev number needs to be done inside
+    # write_version_py(), otherwise the import of scipy.version messes
+    # up the build under Python 3.
+    FULLVERSION = VERSION
+    if os.path.exists('.git'):
+        GIT_REVISION = git_version()
+    elif os.path.exists('orthoproj/version.py'):
+        # must be a source distribution, use existing version file
+        # load it as a separate module to not load scipy/__init__.py
+        import imp
+        version = imp.load_source('orthoproj.version', 'orthoproj/version.py')
+        GIT_REVISION = version.git_revision
+    else:
+        GIT_REVISION = "Unknown"
+
+    if not ISRELEASED:
+        FULLVERSION += '.dev0+' + GIT_REVISION[:7]
+
+    return FULLVERSION, GIT_REVISION
+
+
+def write_version_py(filename='orthoproj/version.py'):
+    cnt = """
+# THIS FILE IS GENERATED FROM ORTHOPROJ SETUP.PY
+short_version = '%(version)s'
+version = '%(version)s'
+full_version = '%(full_version)s'
+git_revision = '%(git_revision)s'
+release = %(isrelease)s
+if not release:
+    version = full_version
+"""
+    FULLVERSION, GIT_REVISION = get_version_info()
+
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % {'version': VERSION,
+                       'full_version': FULLVERSION,
+                       'git_revision': GIT_REVISION,
+                       'isrelease': str(ISRELEASED)})
+    finally:
+        a.close()
+
+
+write_version_py()
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='orthoproj',
     packages=['orthoproj'],
-    version='0.0.2',
+    version=get_version_info()[0],
     description='Python package to create an Orthogonal Projection of 3D '
                 'data with full axes synchronisation',
     long_description=long_description,
